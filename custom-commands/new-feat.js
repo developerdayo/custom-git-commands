555ce38b6739b539d8a02ffe7934f6b426b9c93a
Sarah Ferguson <sarah.an.ferguson@gmail.com>
2019-09-26 01:38:18 -0400

    new feat func

--- custom-commands/new-feat.js
+++ custom-commands/new-feat.js
@@ -10,25 +10,37 @@ const stage = env.stage;
 const prod = env.prod;
 const remotes = env.remotes;
 
-function init() {
+function init(name) {
+    const featName = process.argv[2];
     console.log(
         chalk.yellow('Fetching...')
     );
+
     simpleGitPromise.fetch(remotes)
     .then(
         (successFetch) => {
             console.log(
                 chalk.yellow(`Fetch Successful! Creating new feature branch from branch '${prod}'`)
             );
-            simpleGitPromise.checkout(`${prod}`)
+            simpleGitPromise.raw([
+                'checkout',
+                '-b',
+                `${featName}`,
+                `${prod}`
+            ])
             .then(
                 (successCommit) => {
                     console.log(
-                        chalk.yellow(`checked out ${prod}`)
+                        chalk.yellow(`checked out feat/${featName}`)
                     );
                 }, (failed) => {
                     console.log(
-                        chalk.red(`Failed to create new feature branch!`)
+                        chalk.red(
+                            `Failed to create new feature branch! This happen because:
+                            • you already have a local copy,
+                            • you entered an invalid branch name,
+                            • the branch names specified in env-branch-names.js need to be updated.`
+                        )
                     );
 
             });
